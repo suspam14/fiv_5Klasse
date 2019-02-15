037fdd1b1f82e25d7799828c420c5216ebb5d9fb
suspam14 <suspam14@htlkaindorf.at>
2019-02-15 11:52:15 +0100

    angular

--- ue04/src/app/cars.component.ts
+++ ue04/src/app/cars.component.ts
@@ -6,4 +6,63 @@ import { Component } from '@angular/core';
 })
 export class CarsComponent {
 
+  public barChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+
+  public barChartLabels: string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType = 'bar';
+  public barChartLegend = true;
+
+  public barChartData: any[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+
+  // events
+  public chartClicked(e: any): void {
+    console.log(e);
+  }
+
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
+
+  public randomize(): void {
+    this.updateBars().then( (result: number []) => {
+      console.log('Daten eingetroffen');
+      const clone = JSON.parse(JSON.stringify(this.barChartData));
+      clone[0].data = result;
+      this.barChartData = clone;
+    }).catch( (err) => {console.log(err);
+    });
+  }
+
+  public async randomize2() {
+    try {
+        const result = await this.updateBars();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  private updateBars(): Promise<number []> {
+      return new Promise<number []> ((resolve, reject) => {
+        console.log('Refresh gestartet');
+        setTimeout( () => {
+          // reject(new Error('Fehler aufgetreten'));
+        const data = [
+          Math.round(Math.random() * 100),
+          59,
+          80,
+          (Math.random() * 100),
+          56,
+          (Math.random() * 100),
+          40];
+          resolve(data);
+      }, 2000);
+    });
+
+  }
 }
